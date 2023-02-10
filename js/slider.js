086a542846a3eba847248263d05fd191239ae8d4
Luka Mazione <luka.mazione@gmail.com>
2023-02-10 00:55:03 +0000

    styling arrowa slider

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.header').slick({
+    mobileFirst: true,
+    autoplay: true,
+    infinite: true,
+    slideToShow: 1,
+    slideToScroll: 1,
+    arrows: true,
+    dots: true,
+    });
+  })
